2baa71c242f80005699fc9c722f760e441607679
Shahen Hovhannisyan <shahen@simplytechnologies.net>
2017-01-11 16:25:26 +0400

    updated compression static method

--- index.js
+++ index.js
@@ -7,20 +7,23 @@ class Compressor {
     let currChar;
     let phrase = data[0];
     let code = 256;
-    for (let i = 1; i < data.length; i++) {
-      currChar = data[i];
-      if (dict[phrase + currChar] != null) {
-        phrase += currChar;
-      } else {
-        out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-        dict[phrase + currChar] = code;
-        code++;
-        phrase = currChar;
-      }
+    for (let i=1; i<data.length; i++) {
+        currChar=data[i];
+        if (dict[phrase + currChar] != null) {
+            phrase += currChar;
+        }
+        else {
+            out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
+            dict[phrase + currChar] = code;
+            code++;
+            phrase=currChar;
+        }
     }
     out.push(phrase.length > 1 ? dict[phrase] : phrase.charCodeAt(0));
-
-    return out;
+    for (let i=0; i<out.length; i++) {
+        out[i] = String.fromCharCode(out[i]);
+    }
+    return out.join("");
   }
   static decompress(data) {
     let dict = {};
